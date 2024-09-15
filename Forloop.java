f9f169b7340a1d4d9aca7e6f1f52c0311f21ddd7
PoojaKanagaraj06 <153050125+PoojaKanagaraj06@users.noreply.github.com>
2024-09-15 20:08:14 +0530

    Add files via upload
    I have uploaded a basic For loop program in java

---
+++ Forloop.java
@@ -0,0 +1,11 @@
+class Forloop{
+    public static void main(String[] args)
+    {
+        System.out.println("The numbers from 0 to 5 are:");
+        for (int i=0;i<6;i++){ 
+        
+        System.out.println(i);
+    }
+    }
+
+}
